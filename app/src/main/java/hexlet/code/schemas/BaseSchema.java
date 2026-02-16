b95b434f9ff1d1ecef5130139396e42e18efdb58
user <user@MacBook-Air-user.local>
2026-02-16 16:00:39 +0300

    fixed checkstyle

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,21 +1,40 @@
 package hexlet.code.schemas;
-/** This class is abstract class and man class for all a */
+/**
+ * Base abstract schema class for value validation.
+ *
+ * @param <T> the type of value to validate
+ */
 public abstract class BaseSchema<T> {
 
     private boolean required = false;
 
+    /**
+     * Validates the given value according to schema rules.
+     *
+     * @param value value to validate
+     * @return true if value is valid, false otherwise
+     */
     public abstract boolean validate(T value);
 
-    /** required method is allowing is in other classes and disable null value */
+    /**
+     * Marks the value as required (null is not allowed).
+     *
+     * @return current schema instance
+     */
     public BaseSchema<T> required() {
         this.required = true;
         return this;
     }
 
-    protected boolean isRequired() {
+    protected final boolean isRequired() {
         return required;
     }
-    /** This valid method checking the given value is Valid or not */
+    /**
+     * Checks whether the given value is valid.
+     *
+     * @param value value to validate
+     * @return true if value is valid, false otherwise
+     */
     public boolean isValid(T value) {
         if (value == null) {
             return !required;
