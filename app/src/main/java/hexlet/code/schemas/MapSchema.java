ccfa450418be18f89aba2259d6c7d6325605556e
user <user@MacBook-Air-user.local>
2026-02-16 11:58:04 +0300

    all things finished for the first check

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -2,25 +2,43 @@ package hexlet.code.schemas;
 
 import java.util.Map;
 
-public class MapSchema extends BaseSchema<Map> {
+public class MapSchema extends BaseSchema<Map<String, ?>> {
     private Integer sizeForMap = null;
+    private Map<String, BaseSchema<String>> shapes = Map.of();
 
     @Override
-    public boolean validate(Map value) {
-        if (sizeForMap == null) {
-            return true;
-        }
-        if (sizeForMap != null && value.size() == sizeForMap) {
-            return true;
-        }
-        return false;
+    public MapSchema required() {
+        super.required();
+        return this;
     }
 
+    @Override
+    public boolean validate(Map<String, ?> value) {
+        if (sizeForMap != null && value.size() != sizeForMap) {
+            return false;
+        }
+        if (shapes.isEmpty()) {
+            return true;
+        } else {
+            for (var shape : shapes.entrySet()) {
+                String key = shape.getKey();
+                BaseSchema sch = shape.getValue();
+                Object fieldValue = (value.get(key));
+                if (!sch.isValid(fieldValue)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     public MapSchema sizeof(Integer sizeNum) {
         this.sizeForMap = sizeNum;
         return this;
     }
 
-
+    public MapSchema shape(Map<String, BaseSchema<String>> schemas) {
+        this.shapes = schemas;
+        return this;
+    }
 }
--- app/src/test/java/hexlet/code/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/MapSchemaTest.java
@@ -1,19 +1,21 @@
 package hexlet.code;
 
+import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MapSchemaTest {
+    private final Validator validator = new Validator();
 
     @Test
     void testForNull() {
-        Validator v = new Validator();
-        MapSchema schema = v.map();
+        MapSchema schema = validator.map();
         var data = new HashMap<String, String>();
 
         assertTrue(schema.isValid(null));
@@ -25,8 +27,7 @@ class MapSchemaTest {
 
     @Test
     void testWithRequired() {
-        Validator v = new Validator();
-        MapSchema schema = v.map();
+        MapSchema schema = validator.map();
         var data = new HashMap<String, String>();
 
         assertFalse(schema.required().isValid(null));
@@ -37,8 +38,7 @@ class MapSchemaTest {
 
     @Test
     void testSizeOfForNull() {
-        Validator v = new Validator();
-        MapSchema schema = v.map();
+        MapSchema schema = validator.map();
         schema.sizeof(2);
 
         assertTrue(schema.isValid(null));
@@ -46,8 +46,7 @@ class MapSchemaTest {
 
     @Test
     void testWithSizeOf() {
-        Validator v = new Validator();
-        MapSchema schema = v.map();
+        MapSchema schema = validator.map();
         var data = new HashMap<String, String>();
         schema.sizeof(2);
         data.put("key1", "value1");
@@ -58,8 +57,7 @@ class MapSchemaTest {
 
     @Test
     void testRequiredAndSizeOf() {
-        Validator v = new Validator();
-        MapSchema schema = v.map();
+        MapSchema schema = validator.map();
         var data = new HashMap<String, String>();
         schema.sizeof(2);
         schema.required();
@@ -68,5 +66,45 @@ class MapSchemaTest {
         data.put("key1", "value1");
         data.put("key2", "value2");
         assertTrue(schema.isValid(data));
+    }
+
+    @Test
+    void testShapeWithRequired() {
+        MapSchema schema = validator.map();
+
+        Map<String, String> emptyMap = new HashMap<>();
+        assertTrue(schema.isValid(emptyMap));
+
+        Map<String, BaseSchema<String>> schemas = new HashMap<>();
+        schemas.put("firstName", validator.string().required());
+        schemas.put("lastName", validator.string().required().minLength(2));
+        schema.shape(schemas);
+
+        Map<String, String> human1 = new HashMap<>();
+        human1.put("firstName", "John");
+        human1.put("lastName", "Smith");
+
+        Map<String, String> human2 = new HashMap<>();
+        human2.put("firstName", "John");
+        human2.put("lastName", null);
+
+        assertTrue(schema.isValid(human1));
+        assertFalse(schema.isValid(human2));
+    }
+
+    @Test
+    void testShapeWithAnotherName() {
+        MapSchema schema = validator.map();
+
+        Map<String, BaseSchema<String>> schemas = new HashMap<>();
+        schemas.put("firstName", validator.string().required());
+        schemas.put("lastName", validator.string().required().minLength(2));
+        schema.shape(schemas);
+
+        Map<String, String> human3 = new HashMap<>();
+        human3.put("firstName", "Anna");
+        human3.put("lastName", "B");
+
+        assertFalse(schema.isValid(human3));
     }
 }
